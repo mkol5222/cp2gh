a0b1b4c66fda3a328de7b6b5757a6c5eb14aac71
Martin Koldodovsky <mkoldov@gmail.com>
2024-01-25 13:30:19 +0100

    .

--- cp2gh/src/client/cputils.ts
+++ cp2gh/src/client/cputils.ts
@@ -30,6 +30,14 @@ function objectsById(arrayOfObjects) {
   return dictionaryOfObjects;
 }
 
+Object.defineProperty(Array.prototype, 'flat', {
+  value: function(depth = 1) {
+    return this.reduce(function (flat, toFlatten) {
+      return flat.concat((Array.isArray(toFlatten) && (depth>1)) ? toFlatten.flat(depth-1) : toFlatten);
+    }, []);
+  }
+});
+
 function processRulebase(rulebase, objectsDictionary) {
 
 
